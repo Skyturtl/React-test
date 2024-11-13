e8796866f4401c9188a3b7e1ef91da2923a6fbc5
Skyturtl <112505166+Skyturtl@users.noreply.github.com>
2024-11-13 15:10:52 -0600

    Completed very basic version of the game including state and also status of the game

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -16,6 +16,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   
   function handleClick(i) {
+    if(squares[i] || calculateWinner(squares)){
+      return;
+    }
     const nextSquares = squares.slice();
     if(xIsNext) {
       nextSquares[i] = "X";
@@ -27,8 +30,17 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return(
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
